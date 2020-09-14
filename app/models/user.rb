3a55f148ef9e28c3b0625363b9c7521723b23e6e
AROD <alex@ARODs-MBP.attlocal.net>
2020-09-14 18:13:11 -0500

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  validates :name, presence: true
+  before_save { |user| user.email = user.email.downcase }
+
+  has_many :secrets
+  has_many :likes, dependent: :destroy
+  has_many :secrets_liked, through: :likes, source: :secret
+end
---
+++ spec/controllers/secrets_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+RSpec.describe SecretsController, type: :controller do
+  before do
+    @user = create(:user)
+    @secret = create(:secret, user: @user)
+  end
+  context "when not logged in" do
+    before do
+      session[:user_id] = nil
+    end
+    it "cannot access index" do
+        get :index
+        expect(response).to redirect_to('/sessions/new')
+    end
+    it "cannot access create" do
+        post :create
+        expect(response).to redirect_to('/sessions/new')
+    end
+    it "cannot access destroy" do
+        delete :delete, id: @secret
+        expect(response).to redirect_to('/sessions/new')
+    end
+    context "when signed in as the wrong user" do
+      before do
+        @user2 = create(:user, name:"Brian Duong", email: "brian@dojo.com")
+        @secret2 = create(:secret, user: @user2)
+        session[:user_id] = @user.id
+      end
+      it "cannot destroy another user's secret" do
+        delete :delete, id: @secret2
+        expect(response).to redirect_to("/users/#{@user.id}")
+      end
+    end
+  end
+end
